ce664a1c54d6768e03b24886185ed269142c5905
mrbeanos <59376757+rolss@users.noreply.github.com>
2023-02-15 13:14:44 -0500

    Add files via upload

---
+++ Translator_AllFiles/Translator/src/traductor1/Traductor_TorradoVergara.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package traductor1;
+
+/**
+ *
+ * @author Torrado'sLaptop
+ */
+public class Traductor_TorradoVergara {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Traductor ui = new Traductor();
+        ui.setVisible(true);
+        ui.setBounds(1200, 100, 385, 330);
+    }
+    
+}
